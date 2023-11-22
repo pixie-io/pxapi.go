fa2a0a79da544836a289e8bd5ed576ed1483db3d
Dom Del Nano <ddelnano@pixielabs.ai>
2023-11-22 13:19:44 +0000

    Remove log message used for testing
    Signed-off-by: Dom Del Nano <ddelnano@pixielabs.ai>

--- client.go
+++ client.go
@@ -28,19 +28,15 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/metadata"
 
-	"px.dev/pxapi/proto/cloudpb"
-	"px.dev/pxapi/proto/vizierpb"
 	"px.dev/pxapi/types"
 	"px.dev/pxapi/utils"
+	"px.dev/pxapi/proto/cloudpb"
+	"px.dev/pxapi/proto/vizierpb"
 )
 
 const (
 	defaultCloudAddr = "work.withpixie.ai:443"
 )
-
-func init() {
-	fmt.Println("Using new client")
-}
 
 // TableRecordHandler is an interface that processes a table record-wise.
 type TableRecordHandler interface {
